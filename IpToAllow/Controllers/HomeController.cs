e5debec4c0564a1485a4f333dc1c97821ba0c51a
Azib <Azib@IMRANSAEED>
2022-11-16 19:09:37 +0500

    initial work

---
+++ IpToAllow/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using IpToAllow.Filters;
+using IpToAllow.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+
+namespace IpToAllow.Controllers
+{
+    [IPAuthorization]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ IpToAllow/Filters/IPAuthorization.cs
@@ -0,0 +1,84 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace IpToAllow.Filters
+{
+
+    public class IPAuthorization : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            IpWork.CheckIp(filterContext);
+        }
+    }
+    public static class IpWork
+    {
+        public static bool CheckIp(ActionExecutingContext filterContext)
+        {
+            string IpAddress = GetLocalIPAddress();
+
+            IConfigurationRoot _config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+            .AddJsonFile("appsettings.json", false)
+            .Build();
+
+            List<string> ip_addresses = _config.GetSection("IsAllowed:IpAddresses").Get<List<string>>();
+            bool res = ip_addresses.Where(a => a.Trim().Equals(IpAddress, StringComparison.InvariantCultureIgnoreCase)).Any();
+
+            if(!res)
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary { {"controller","Error"},{"action","Index"} });
+            }
+
+            return res;
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+    }
+
+  
+
+
+    //public class IpWork
+    //{ 
+    //    public static string GetLocalIPAddress()
+    //    {
+    //        var host = Dns.GetHostEntry(Dns.GetHostName());
+    //        foreach (var ip in host.AddressList)
+    //        {
+    //            if (ip.AddressFamily == AddressFamily.InterNetwork)
+    //            {
+    //                return ip.ToString();
+    //            }
+    //        }
+    //        throw new Exception("No network adapters with an IPv4 address in the system!");
+    //    }
+
+    //    public static bool checkIpAddressIsValid(ActionExecutedContext filterContext)
+    //    {
+    //        IConfigurationRoot _config = new ConfigurationBuilder()
+    //        .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+    //        .AddJsonFile("appsettings.json", false)
+    //        .Build();
+
+    //        List<string> ip_addresses = _config.GetSection("IsAllowed:IpAddresses").Get<List<string>>();
+    //        bool res = ip_addresses.Where(a => a.Trim().Equals(IpAddress, StringComparison.InvariantCultureIgnoreCase)).Any();
+
+    //        return res;
+    //    }
+    //}
+}
