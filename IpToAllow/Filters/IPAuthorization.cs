27ba231cf34572718d55fbe984c95da1d1c9f3de
bilal khalid <bilal.khalid@braincorp.net>
2022-11-17 10:11:39 +0500

    Removing Unecessary things.

--- IpToAllow/Filters/IPAuthorization.cs
+++ IpToAllow/Filters/IPAuthorization.cs
@@ -27,12 +27,10 @@ namespace IpToAllow.Filters
 
             List<string> ip_addresses = _config.GetSection("IsAllowed:IpAddresses").Get<List<string>>();
             bool res = ip_addresses.Where(a => a.Trim().Equals(IpAddress, StringComparison.InvariantCultureIgnoreCase)).Any();
-
             if(!res)
             {
                 filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary { {"controller","Error"},{"action","Index"} });
             }
-
             return res;
         }
 
@@ -49,36 +47,4 @@ namespace IpToAllow.Filters
             throw new Exception("No network adapters with an IPv4 address in the system!");
         }
     }
-
-  
-
-
-    //public class IpWork
-    //{ 
-    //    public static string GetLocalIPAddress()
-    //    {
-    //        var host = Dns.GetHostEntry(Dns.GetHostName());
-    //        foreach (var ip in host.AddressList)
-    //        {
-    //            if (ip.AddressFamily == AddressFamily.InterNetwork)
-    //            {
-    //                return ip.ToString();
-    //            }
-    //        }
-    //        throw new Exception("No network adapters with an IPv4 address in the system!");
-    //    }
-
-    //    public static bool checkIpAddressIsValid(ActionExecutedContext filterContext)
-    //    {
-    //        IConfigurationRoot _config = new ConfigurationBuilder()
-    //        .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
-    //        .AddJsonFile("appsettings.json", false)
-    //        .Build();
-
-    //        List<string> ip_addresses = _config.GetSection("IsAllowed:IpAddresses").Get<List<string>>();
-    //        bool res = ip_addresses.Where(a => a.Trim().Equals(IpAddress, StringComparison.InvariantCultureIgnoreCase)).Any();
-
-    //        return res;
-    //    }
-    //}
 }
