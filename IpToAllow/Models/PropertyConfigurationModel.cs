b0cb5a41277c747f0b318a961f1012f0de0fb2fd
Azib <Azib@IMRANSAEED>
2022-11-18 19:38:06 +0500

    js and css file add propertyconfigmodel add

---
+++ IpToAllow/Models/PropertyConfigurationModel.cs
@@ -0,0 +1,18 @@
+﻿namespace IpToAllow.Models
+{
+    public class PropertyConfigurationModel
+    {
+        public string XmlLabel { get; set; }
+        public bool IsRequired { get; set; }
+        public string Value { get; set; }
+        public bool IsList { get; set; }
+        public ValueCollectionType ValueType { get; set; }
+        public char Delimeter { get; set; }
+    }
+    public enum ValueCollectionType
+    {
+        Normal = 1,
+        List = 2,
+        Dictionary = 3,
+    }
+}
